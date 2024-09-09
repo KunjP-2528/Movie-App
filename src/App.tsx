5b4e6b6f6c0e899b1dd535968f3ff07ded87d9a2
kunjP_2528 <165396689+KunjP-2528@users.noreply.github.com>
2024-09-09 23:45:32 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Watchlist from './pages/WatchList';
+import Favorite from './pages/Favorite';
+import Header from './component/Header';
+import './Style/App.css';
+import Hero from './component/Hero';
+import SearchResults from './pages/SearchResult';
+import useTheme from './component/CustomHook';
+
+const App: React.FC = () => {
+  const { theme, toggleTheme } = useTheme();
+  console.log(theme)
+
+  return (
+    <div className={`app-container ${theme}`}>
+      <Router>
+        <Header theme={theme} toggleTheme={toggleTheme} />
+        <Hero />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/watchlist" element={<Watchlist />} />
+          <Route path="/favorites" element={<Favorite />} />
+          <Route path="/search" element={<SearchResults />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
